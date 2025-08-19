c0a225035804912e9d7a9bd1a7c03eb3d805e357
Manuela Andrade <manuela.andrade894@gmail.com>
2025-08-19 10:43:23 -0300

    PersonagemRpg.java

--- Encapsulamento-Aula01/src/personagemRpg/PersonagemRpg.java
+++ Encapsulamento-Aula01/src/personagemRpg/PersonagemRpg.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class PersonagemRpg {
 	private String nome;
-	private String[] classe = { "Guerreiro", "Elfo", "Bruxa" };
-	private int poder;
+	private String[] classe = { "Guerreiro", "Elfo", "Bruxa" }; //Array das classes opcionais pelo usuário
+	private int poder; 
 	private int vida;
 	private int personagemEscolhido;
-	public boolean validacao = false;
+	public boolean validacao = false; //variável pulica de validação while para que o cod possa encerrar-se
 
 	Scanner sc = new Scanner(System.in);
 
@@ -25,7 +25,7 @@ public class PersonagemRpg {
 		int opcoes = 0;
 		System.out.println("Escolha a classe do seu personagem");
 		System.out.println("[ 1 ]- " + classe[0] + "  " + "[ 2 ]- " + classe[1] + "  " + "[ 3 ]- " + classe[2]);
-
+		//checagem de resposta do Scanner para a classe
 		do {
 			opcoes = sc.nextInt();
 			if (opcoes < 1 || opcoes > 3) {
@@ -46,6 +46,7 @@ public class PersonagemRpg {
 		do {
 			System.out.println("Quantidade de poder para o(a) " + (classe[personagemEscolhido]) + " " + nome + ": ");
 			poderEscolhido = sc.nextInt();
+			//impede que o poder seja menor que 1 e maior que 100 
 			if (poderEscolhido < 1 || poderEscolhido > 100) {
 				System.out.println("O poder precisa estar entre 1 e 100");
 			} else {
@@ -59,7 +60,9 @@ public class PersonagemRpg {
 	}
 
 	public void setVida() {
+		//impede que a vida seja menor ou igual a 0 ou maior que 100
 		int qntVida = 0;
+		//loop para a checagem de inserção de dados
 		do {
 			System.out.println("Quantidade de vida:");
 			qntVida = sc.nextInt();
@@ -73,7 +76,7 @@ public class PersonagemRpg {
 
 	public void atualizacaoVida() {
 		int qntVida = 0;
-
+		//Sugestão de atualização das caracteristicas vida e poder do personagem
 		while (!validacao) {
 			System.out.println("Quantidade de vida:");
 			qntVida = sc.nextInt();
@@ -81,7 +84,8 @@ public class PersonagemRpg {
 			if (qntVida >= 1) {
 				qntVida = this.vida;
 			} else if (qntVida > 100) {
-				System.out.println("100 de vida é o máximo. seu personagem está curado!");
+				//A variável vida guarda o valor 100 caso o Scanner leia um número maior que 100
+				System.out.println("100 de vida aplicado. Seu personagem está curado!");
 				this.vida = 100;
 			} else if (qntVida == 0) {
 				System.out.println(">>" + (classe[personagemEscolhido]) + " chegou ao fim da vida 💀");
@@ -92,6 +96,7 @@ public class PersonagemRpg {
 	}
 
 	public void personagemFinal() {
+		//Amostragem dos dados do personagem final
 		System.out.printf(
 				">> Personagem Criado!\nNome: %s%nClasse: " + classe[personagemEscolhido] + " \nPoder:%d%nVida:%d%n",
 				nome, poder, vida);
@@ -103,7 +108,7 @@ public class PersonagemRpg {
 	}
 
 	public void atualizarPersonagem() {
-
+//Atualização dos dados vida e poder
 		while (!validacao) {
 			System.out.println("Atualizar personagem? S/N");
 			String resposta1 = sc.next();
