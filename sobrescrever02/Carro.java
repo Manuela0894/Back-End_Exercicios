c1954ab3c8a5c1595314a83906a09801d0fbe650
Manuela Andrade <manuela.andrade894@gmail.com>
2025-08-20 11:03:59 -0300

    Add files via upload
    Usando o Override em heranças

---
+++ sobrescrever02/Carro.java
@@ -0,0 +1,9 @@
+package sobrescrever02;
+
+public class Carro extends Veiculo{
+
+	@Override
+	public String ligar() {
+		return "O carro está iniciando...";
+	}
+}
---
+++ sobrescrever02/Veiculo.java
@@ -0,0 +1,9 @@
+package sobrescrever02;
+
+public class Veiculo {
+
+	public String ligar() {
+		return "O veículo está iniciando...";
+	}
+	
+}
