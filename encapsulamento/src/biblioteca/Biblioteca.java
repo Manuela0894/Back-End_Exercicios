563212561fa3ad830ae11467b51b6394c216779b
Manuela0894 <manuela.andrade894@gamil.com>
2025-08-22 19:57:15 -0300

    renomeando a pasta encapsulamento

---
+++ encapsulamento/src/biblioteca/Biblioteca.java
@@ -0,0 +1,63 @@
+package biblioteca;
+
+import java.util.Scanner;
+
+public class Biblioteca {
+	
+		 private String nomeLivro;
+		 private String nomeAutor;
+		 private int numPaginas;
+		 
+		 Scanner sc = new Scanner(System.in);
+		 
+		 public String getNomeLivro() {
+			return nomeLivro;
+		 }
+		 public void setNomeLivro() {
+			 System.out.println("Informe o nome do livro:");
+			this.nomeLivro = sc.nextLine(); }
+			
+		 public String getNomeAutor() {
+			return nomeAutor;
+		 }
+		 public void setNomeAutor() {
+			 System.out.println("Informe o nome do Autor:");
+				this.nomeAutor = sc.nextLine();
+			
+		 }
+		 
+		 public int getNumPaginas() {
+			return numPaginas;
+		 }
+		 public void setNumPaginas() {
+			 int num;
+		        do {
+		            System.out.println("Informe o número de páginas:");
+		            num = sc.nextInt();
+		            if (num < 5) {
+		                System.out.println("Quantidade de páginas inválida!");
+		            } else {
+		                this.numPaginas = num;
+		            }
+		        } while (num < 5);  
+		    }
+		 
+		 	public void encomendarLivro(){
+		 		String respostaEncomenda;
+		 		System.out.println("Confirma sua escolha de encomenda? S/N");
+		 		sc.nextLine();
+		 		respostaEncomenda = sc.nextLine();
+		 		
+		 		switch(respostaEncomenda.toLowerCase()) {
+		 		case "s": System.out.println("Livro adicionado com sucesso!");
+		 		break;
+		 		case "n": System.out.println("Encomenda cancelada.\nObrigada por usar o sistema!");
+		 		break;
+		 		default:
+		 			System.out.println("Resposta inválida! Por favor Digite S ou N.");
+		 			break;
+		 		}
+		 		
+		 	}
+
+}
---
+++ encapsulamento/src/personagemRpg/Main.java
@@ -0,0 +1,15 @@
+package personagemRpg;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		PersonagemRpg p = new PersonagemRpg();
+		
+		p.setNome();
+		p.escolhaClasse();
+		p.setPoder();
+		p.setVida();
+		p.personagemFinal();
+		
+	}
+}
